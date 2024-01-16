3541b0345f8c4113ea491f5fde41067c1cae0f4a
Vishnu CP <vishnucpmcd@gmail.com>
2024-01-16 13:01:48 +0530

    login and register controllers updated, password hash done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,13 +8,14 @@ function isEmailTaken(email) {
     return users.some((user) => user.email === email);
 }
 
-exports.userRegister = (req, res) => {
+exports.userRegister = async (req, res) => {
     const { name, email, password } = req.body;
 
-    // const hashedPass =  bcrypt.hash(password, saltRound);
+    const hashedPass = await bcrypt.hash(password, saltRound);
+    
     
     if (isEmailTaken(email)) {
-        res.status(400).json({
+        return res.status(400).json({
             success: false,
             message: 'Email is already taken'
         });
@@ -23,7 +24,7 @@ exports.userRegister = (req, res) => {
         const newUser = {
             name: name,
             email: email,
-            password: password
+            password: hashedPass
         }
 
         console.log(newUser);
@@ -36,25 +37,39 @@ exports.userRegister = (req, res) => {
     }
 }
 
-exports.userLogin = (req, res) => {
+exports.userLogin = async (req, res) => {
     const { email, password } = req.body;
 
-    const user = users.find((u) => u.email === email && u.password === password);
-    console.log(users);
+    
+
+    const user = users.find((u) => u.email === email);
+    //finding user with same email
     console.log("user", user);
 
+   
 
-    if (user) {
-        res.status(200).json({
-            success: true,
-            message: "Login Successfull",
-            isAuthenticated: true,
-            user
+    if(!user){
+        return res.status(401).json({
+            success:false,
+            message:"User not found!"
+        });
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    //checking password and encrypted password
+
+
+    if (!isValid) {
+        res.status(500).json({
+            success: false,
+            message: "Invalid Credentials",
         });
     } else {
-        res.status(401).json({
-            success: false,
-            message: "Invalid Credentials!"
+        res.status(200).json({
+            success: true,
+            message: "Login Successfull!",
+            isAuthenticated: true,
+            user
         });
     }
 
